78787d5be60c4a16c9636ce8fd659abf41859430
home <timurfedorov042@gmail.com>
2025-02-12 13:22:09 +0300

    new module widget

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,10 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Маскирует номер банковской карты по правилу XXXX XX** **** XXXX."""
     card_number_str = str(card_number)
     return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """Маскирует номер банковского счета по правилу **XXXX."""
     account_number_str = str(account_number)
     return "**" + account_number_str[-4:]
--- src/widget.py
+++ src/widget.py
@@ -2,30 +2,30 @@ from src import masks
 
 
 def mask_account_card(card_inf: str) -> str:
-    '''Обрабатывает информацию как о картах, так и о счетах
-    Возвращает строку с замаскированным номером'''
+    """Обрабатывает информацию как о картах, так и о счетах
+    возвращает строку с замаскированным номером"""
     index = 0
     for i in card_inf:
         if i.isdigit():
             break
         else:
             index += 1
-    if card_inf[:index-1] == 'Счет':
-        return card_inf[:index-1]+' '+masks.get_mask_account(card_inf[index:])
+    if card_inf[: index - 1] == "Счет":
+        return card_inf[: index - 1] + " " + masks.get_mask_account(card_inf[index:])
     else:
-        return card_inf[:index-1]+' '+masks.get_mask_card_number(card_inf[index:])
+        return card_inf[: index - 1] + " " + masks.get_mask_card_number(card_inf[index:])
 
 
-def get_date(date:str) -> str:
-    '''Функция которая принимает на вход строку с датой в формате
-       "2024-03-11T02:26:18.671407"
-       и возвращает строку с датой в формате "ДД.ММ.ГГГГ" '''
-    year,month,day = date[:10].split('-')
-    return f'{day}.{month}.{year}'
+def get_date(date: str) -> str:
+    """Функция, которая принимает на вход строку с датой в формате
+    "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате 'ДД.ММ.ГГГГ'"""
+    year, month, day = date[:10].split("-")
+    return f"{day}.{month}.{year}"
 
 
-print(mask_account_card('Maestro 1596837868705199'))
-print(mask_account_card('Visa Classic 6831982476737658'))
-print(mask_account_card('Visa Gold 5999414228426353'))
-print(mask_account_card('Счет 73654108430135874305'))
-print(get_date('2024-03-11T02:26:18.671407'))
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(get_date("2024-03-11T02:26:18.671407"))

ab586bd785489df1f99702637660da6261b2f638
home <timurfedorov042@gmail.com>
2025-02-20 10:25:47 +0300

    HW-3

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,15 +2,22 @@ import pytest
 
 from src.masks import get_mask_account, get_mask_card_number
 
+# Тестирование функции mask_card_number
+def test_mask_card_number() -> None:
+    """
+    Тестирование функции mask_card_number. Проверяется, что номер карты правильно маскируется.
+    """
+    # Проверка корректного номера карты
+    assert get_mask_card_number("1234567891234567") == "1234 56** **** 4567"
+    # Проверка некорректного номера карты
+    assert get_mask_card_number("12345") == "Некорректный номер карты"
 
-@pytest.fixture
-def masks_card() -> str:
-    return "5385882263548596"
-
-
-def test_masks_card(masks_card: str) -> None:
-    assert get_mask_card_number(masks_card) == "5385 88 ** 8596"
-
-
-def test_masks_account() -> None:
-    assert get_mask_account("96354521441242052365") == "**2365"
+# Тестирование функции mask_account_number
+def test_mask_account_number() -> None:
+    """
+    Тестирование функции mask_account_number. Проверяется, что номер счета правильно маскируется.
+    """
+    # Проверка корректного номера счета
+    assert get_mask_account("76666108430178874305") == "**4305"
+    # Проверка некорректного номера счета
+    assert get_mask_account("234") == "Некорректный номер счета"
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -14,9 +14,12 @@ def get_dates() -> str:
         ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79 ** 6361"),
     ],
 )
+
 def test_mask_account_card(data: str, result: str) -> None:
     assert mask_account_card(data) == result
 
+def test_get_date(date_input: str) -> None:
+    assert get_date(date_input) == "03.07.2019"
 
-def test_get_date(get_dates: str) -> None:
-    assert get_date(get_dates) == "03.07.2019"
+def test_get_date_2(date_input: str) -> None:
+    assert get_date(date_input) == "05.11.2015"
